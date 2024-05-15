b607e15b871a155c0457385d9d2516487f168b42
erwanclx <erwanclou@gmail.com>
2024-05-15 16:16:46 +0200

    Create simulation of emprunt and return

--- Java/src/App.java
+++ Java/src/App.java
@@ -4,20 +4,36 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<Personne> personnes = new ArrayList<Personne>();
+
         Membre membre1 = new Membre("Cloux", "Erwan", 1, new Date(), "");
         Employe employe2 = new Employe("Fleury", "Sébastien", 2, "Directeur", 5000.17);
 
         Livre firstLivre = new Livre("Le petit chaperon rouge", "Erwan", "3472");
+
+        personnes.add(membre1);
+        personnes.add(employe2);
 
         for (int i = 0; i < personnes.size(); i++) {
             personnes.get(i).afficherDetails();
             System.out.println("===============");
         }
 
-        personnes.add(membre1);
-        personnes.add(employe2);
-
         membre1.emprunter(firstLivre);
+        System.out.println("");
+        firstLivre.afficherDetails();
+        System.out.println("===============");
+
+        for (int i = 0; i < personnes.size(); i++) {
+            personnes.get(i).afficherDetails();
+            System.out.println("===============");
+        }
+
+        System.out.println("=== Retour du livre ===");
+
+        membre1.retourner(firstLivre);
+        System.out.println("");
+        firstLivre.afficherDetails();
+        System.out.println("===============");
 
         for (int i = 0; i < personnes.size(); i++) {
             personnes.get(i).afficherDetails();
