a5548b32aef61e042d196085778d36f231948019
erwanclx <erwanclou@gmail.com>
2024-05-15 16:09:34 +0200

    refacto with array

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,13 +1,26 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Personne[] personnes = new Personne[2];
-        personnes[0] = new Membre("Cloux", "Erwan", 1, new Date(), "");
-        personnes[1] = new Employe("Fleury", "Sébastien", 2, "Directeur", 5000.17);
+        ArrayList<Personne> personnes = new ArrayList<Personne>();
+        Membre membre1 = new Membre("Cloux", "Erwan", 1, new Date(), "");
+        Employe employe2 = new Employe("Fleury", "Sébastien", 2, "Directeur", 5000.17);
 
-        for (int i = 0; i < personnes.length; i++) {
-            personnes[i].afficherDetails();
+        Livre firstLivre = new Livre("Le petit chaperon rouge", "Erwan", "3472");
+
+        for (int i = 0; i < personnes.size(); i++) {
+            personnes.get(i).afficherDetails();
+            System.out.println("===============");
+        }
+
+        personnes.add(membre1);
+        personnes.add(employe2);
+
+        membre1.emprunter(firstLivre);
+
+        for (int i = 0; i < personnes.size(); i++) {
+            personnes.get(i).afficherDetails();
             System.out.println("===============");
         }
 
