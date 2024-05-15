15133fe4e14e8c3e61b93f3f148c11b527e1d130
erwanclx <erwanclou@gmail.com>
2024-05-15 15:45:09 +0200

    Swap to date creation for example

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,7 +1,9 @@
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Personne[] personnes = new Personne[2];
-        personnes[0] = new Membre("Cloux", "Erwan", 1, "15/05/2024", "");
+        personnes[0] = new Membre("Cloux", "Erwan", 1, new Date(), "");
         personnes[1] = new Employe("Fleury", "Sébastien", 2, "Directeur", 5000.17);
 
         for (int i = 0; i < personnes.length; i++) {
